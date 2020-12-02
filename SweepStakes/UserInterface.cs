cdccad50cde383786df08d12b4007e6201ca24c4
phher802 <your_email@example.com>
2020-12-02 16:20:43 -0600

    Create code for GetUserInputFor method

--- SweepStakes/UserInterface.cs
+++ SweepStakes/UserInterface.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SweepStakes
 {
@@ -15,7 +17,28 @@ namespace SweepStakes
         // methods (can do)
         public static String GetUserInputFor(string prompt)
         {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+            string fName = "";
+            string lName = "";
+      
 
+            Console.WriteLine(prompt);
+            
+            string input = Console.ReadLine();
+
+            if ((prompt == fName) || (prompt == lName) )
+            {
+                return (textInfo.ToTitleCase(input));
+            }
+            else
+            {
+                return input;
+            }
+
+          
+            //Console.WriteLine(textInfo.ToTitleCase(input));
+            //return (textInfo.ToTitleCase(input));
         }
     }
 }
