4aef447d23238c200bdf39dc29c250138d988736
phher802 <your_email@example.com>
2020-12-02 09:45:06 -0600

    Create Constestant class with member variables

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+        // member variables (has a)
+        public string fName;
+        public string lName;
+        public string email;
+        public string address;
+        public int registrationNumber;
+
+        // constructor (spawner)
+        public Contestant()
+        {
+
+        }
+
+        
+        // methods (can do)
+
+
+
+    }
+}
