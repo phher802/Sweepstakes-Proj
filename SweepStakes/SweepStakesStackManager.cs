847a2dcee3d1e5e3f3d13cb12cacbd0a99c86b89
phher802 <your_email@example.com>
2020-12-03 15:50:04 -0600

    Create GetSweepstakes method in SweepstakesStackManager class

--- SweepStakes/SweepStakesStackManager.cs
+++ SweepStakes/SweepStakesStackManager.cs
@@ -14,19 +14,28 @@ namespace SweepStakes
         // constructor (spawner)
         public SweepStakesStackManager()
         {
-
+            Stack<Sweepstakes> stack = new Stack<Sweepstakes>();
         }
 
         // method (can do)
         public void InsertSweepstakes(Sweepstakes sweepstakes)
         {
-
+            stack.Push(sweepstakes);
         }
 
-        //public Sweepstakes GetSweepstakes()
-        //{
+        public Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes sweepstakes;
 
-        //}
+            if (stack.Count > 0)
+            {
+                sweepstakes = stack.Pop();
+                return sweepstakes;
+            }
+
+
+            throw new Exception("Stack is Empty.");
+        }
 
     }
 }
