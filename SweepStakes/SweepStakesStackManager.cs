464f87c3679c046a28bba4aa031a6f42bda50653
phher802 <your_email@example.com>
2020-12-03 16:37:23 -0600

    Add comment notes to explain how ISweepstakes interface is used in SweepstakesStackManager Class

--- SweepStakes/SweepStakesStackManager.cs
+++ SweepStakes/SweepStakesStackManager.cs
@@ -18,11 +18,14 @@ namespace SweepStakes
         }
 
         // method (can do)
+        //Use an abstract methods from the ISweepstakes interface to create a method to add to the stack
         public void InsertSweepstakes(Sweepstakes sweepstakes)
         {
             stack.Push(sweepstakes);
         }
 
+        //is is also another abstract method from the ISweekstakes interface, used to place the last item in the stack
+        // and throws an exception if the stack is empty.
         public Sweepstakes GetSweepstakes()
         {
          
